a28c526eb8da8c5fb6c1383d8adbd79bf6fcd24b
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-08 23:08:15 -0300

    Add label welcome

---
+++ lib/view/widgets/label/label_welcome.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:ink_estimator/themes/colors.dart';
+import 'package:ink_estimator/view/widgets/label/label_base.dart';
+
+class LabelWelcome extends LabelBase {
+  LabelWelcome({
+    Key? key,
+    required label,
+  }) : super(
+            key: key,
+            label: label,
+            fontSize: 28,
+            height: 1.5,
+            fontWeightType: FontWeight.w800,
+            color: AppColors.neutrals.shade200,
+            textAlign: TextAlign.start
+            );
+}
