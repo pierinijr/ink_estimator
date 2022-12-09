01afe5f1331ff8e89dc56b0a42134acac31d1b3f
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-08 23:04:39 -0300

    Update colors

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -4,4 +4,13 @@ class AppColors {
   static const primaryColor = Color(0xFF434E6E);
 
   static const secondaryColor = Color(0xFFE3E9F9);
+
+  static const filterColor = Color(0xFF282A51);
+
+  static const neutrals = MaterialColor(0xFFFFFFFF, neutralsShades);
+
+  static const Map<int, Color> neutralsShades = {
+    200: Color(0xFFFFFFFF),
+    900: Color(0xFF000000),
+  };
 }
