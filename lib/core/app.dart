731a3ea3bcfba0ec0f8cde9e8565191593dd8b36
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-08 23:03:13 -0300

    Remove debug banner

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -13,6 +13,7 @@ class InkEstimator extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       initialRoute: "/welcome",
       routes: routes,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
