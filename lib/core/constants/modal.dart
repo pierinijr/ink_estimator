a380007746ad59fbd4682f41ef8fe4eba034d430
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-10 18:16:23 -0300

    Add new constants files

---
+++ lib/core/constants/modal.dart
@@ -0,0 +1,11 @@
+class ConstantsModal {
+  static ConstantsModalImages get images => ConstantsModalImages();
+}
+
+class ConstantsModalImages {
+  String get heightModalImage => 'lib/assets/height_modal_image.png';
+  String get widthModalImage => 'lib/assets/width_modal_image.png';
+  String get doorsModalImage => 'lib/assets/doors_modal_image.png';
+  String get windowsModalImage => 'lib/assets/windows_modal_image.png';
+  String get areaModalImage => 'lib/assets/area_modal_image.png';
+}
