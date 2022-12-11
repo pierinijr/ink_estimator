034e3ea0a52245bdf51837091c1cbbba0776a2ae
Luciano <l.a.p.pimenta.j@gmail.com>
2022-12-11 20:50:29 -0300

    Add saved values ​​to measurement card

--- lib/view/widgets/cards/card_measurement.dart
+++ lib/view/widgets/cards/card_measurement.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ink_estimator/core/constants/constants.dart';
+import 'package:ink_estimator/model/room_model.dart';
 import 'package:ink_estimator/themes/colors.dart';
 import 'package:ink_estimator/view/widgets/label/label_h2.dart';
 import 'package:ink_estimator/view/widgets/modal/modal_card.dart';
@@ -7,7 +8,11 @@ import 'package:ink_estimator/view_model/room_view_model.dart';
 import 'package:provider/provider.dart';
 
 class CardMeasurement extends StatefulWidget {
-  const CardMeasurement({super.key, required this.index, required this.image, required this.label});
+  const CardMeasurement(
+      {super.key,
+      required this.index,
+      required this.image,
+      required this.label});
 
   final int index;
   final String image;
@@ -20,6 +25,11 @@ class CardMeasurement extends StatefulWidget {
 class _CardMeasurementState extends State<CardMeasurement> {
   @override
   Widget build(BuildContext context) {
+    RoomViewModel roomViewModel = context.watch<RoomViewModel>();
+    RoomModel model = roomViewModel.listData.firstWhere(
+        (element) => element.index == widget.index,
+        orElse: () => RoomModel(index: widget.index));
+
     return GestureDetector(
       onTap: () => {
         Provider.of<RoomViewModel>(context, listen: false)
@@ -65,9 +75,9 @@ class _CardMeasurementState extends State<CardMeasurement> {
                           LabelH2(
                               label: widget.label,
                               fontWeightType: FontWeight.w700),
-                          const LabelH2(
-                              label: "1 porta e 1 janela",
-                              fontWeightType: FontWeight.w300),
+                          LabelH2(
+                              label: model.infos,
+                              fontWeightType: FontWeight.w400),
                         ],
                       ),
                     ),
@@ -75,14 +85,15 @@ class _CardMeasurementState extends State<CardMeasurement> {
                       padding:
                           EdgeInsets.only(right: Constants.spacings.spacing12),
                       child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.end,
-                        children: const [
+                        children: [
                           LabelH2(
-                              label: "100m2", fontWeightType: FontWeight.w700),
+                              label: "${model.area.toStringAsFixed(2)}m²", 
+                              fontWeightType: FontWeight.w700),
                           LabelH2(
-                              label: "10 X 10",
-                              fontWeightType: FontWeight.w300),
+                              label: model.dimensions,
+                              fontWeightType: FontWeight.w400),
                         ],
                       ),
                     ),
